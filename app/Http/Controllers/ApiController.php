9ac4f8611d2690ed8033d2b9214c7f7653af39f1
Ben Pritchard <ben@enrollmentresources.com>
2018-10-25 15:24:23 -0700

    Increase number of pages

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -35,7 +35,7 @@ class ApiController extends Controller
 			]);
 		}
 
-		$recentlyUpdatedPages = $this->entityRepo->getRecentlyUpdated('page', 8);
+		$recentlyUpdatedPages = $this->entityRepo->getRecentlyUpdated('page', 10);
 		$books = $this->entityRepo->getAll('book');
 
 		foreach($recentlyUpdatedPages as $page) {
